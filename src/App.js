ceef75755d11b70e457bccd28fa165e15c4afdfc
Richard McGirt <richardmcgirt@Richards-Mini.attlocal.net>
2023-09-13 13:07:46 -0400

    add counter echo and margin

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
 
 function App() {
+  let [counter, changeCounter] = useState(1)
+  window.changeCounter = changeCounter
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <hr />
+      <h2>{counter}</h2>
+      <h3>{counter}</h3>
+      <h4>{counter}</h4>
+      <h5>{counter}</h5>
+      <h6>{counter}</h6>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
